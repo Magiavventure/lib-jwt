66006571c75278b385861c67dee28a57ae4f490d
marco-polverini-dev <128422361+marco-polverini-dev@users.noreply.github.com>
2024-01-17 20:31:07 +0100

    refactor: changed common version - removed key from JwtException (#3)

--- src/main/java/it/magiavventure/jwt/error/JwtException.java
+++ src/main/java/it/magiavventure/jwt/error/JwtException.java
@@ -4,8 +4,6 @@ import it.magiavventure.common.error.MagiavventureException;
 import it.magiavventure.common.model.Error;
 
 public class JwtException extends MagiavventureException {
-    public static final String JWT_EXPIRED = "jwt-expired";
-    public static final String JWT_NOT_VALID = "jwt-not-valid";
     public JwtException(Error error) {
         super(error);
     }
